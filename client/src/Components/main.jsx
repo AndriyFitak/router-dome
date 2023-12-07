fabbb24c67a904318c69ad3b6aee813b33f1c843
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-12-07 18:55:11 +0200

    js

--- client/src/Components/main.jsx
+++ client/src/Components/main.jsx
@@ -1,13 +1,77 @@
-// main.jsx
-import React from 'react';
-import Home from "../images/homeimg.jpg";
+
 import "../styles/main.css"
+import React, { useState } from 'react';
+
 const Main = () => {
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct = () => {
+    const newProduct = {
+      name: productName,
+      price: productPrice,
+      description: productDescription,
+    };
+
+    setProducts([...products, newProduct]);
+
+    setProductName('');
+    setProductPrice('');
+    setProductDescription('');
+  };
+
   return (
-    <div>
-      <img className='img-home' src={Home} alt="" />
-      <h1 className='sitegit-text-home'>Site <span className='git-text'>Git</span></h1>
-      <p>This is the home page content.</p>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>Форма створення продукту</h1>
+      <div>
+        <label className="form-input name">
+          
+          <input
+            type="text"
+            value={productName}
+            placeholder="Name"
+            onChange={(e) => setProductName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label className="form-input">
+          
+          <input
+            type="text"
+            value={productPrice}
+            placeholder="Price"
+            onChange={(e) => setProductPrice(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label className="form-input description">
+          
+          <textarea
+            value={productDescription}
+            placeholder="Description"
+            onChange={(e) => setProductDescription(e.target.value)}
+          />
+        </label>
+      </div>
+      <button onClick={handleAddProduct} className="button">
+        Додати продукт
+      </button>
+
+      <div>
+        <h2 style={{ textAlign: 'center', marginTop: '30px' }}>Список продуктів</h2>
+        <ul className="product-list">
+          {products.map((product, index) => (
+            <li key={index} className="product-item">
+              <strong>{product.name}</strong> - {product.price} грн
+              <p>{product.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
